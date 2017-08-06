cd4858c8a16f14cab70e402c65565b13956664eb
YoshikiKobayashi <attack_no22001@yahoo.co.jp>
2017-08-06 10:55:17 +0900

    URLを動的に取得できるようにする

--- app/scripts/qiita.js
+++ app/scripts/qiita.js
@@ -11,21 +11,23 @@ const vfs_fonts = require('pdfmake/build/vfs_fonts.js');
     let buttonElement   = document.createElement("button")
     let iconElement     = document.createElement("i")
     downloadElement.className = "slide_controller_fullscreen slide_controller_download"
+    downloadElement.id = "slide_download"
     buttonElement.className = "slide_controller_btn clickable"
     iconElement.className = "fa fa-download"
     buttonElement.appendChild(iconElement)
     downloadElement.appendChild(buttonElement)
 
     //イベント追加
-    downloadElement.addEventListener("click", clickDownload)
+    downloadElement.addEventListener("click", clickDownload, true)
     //downloadElement.insertBefore(buttonElement.insertBefore(iconElement, buttonElement.firstChild), downloadElement.firstChild)
     slide_controller_fullscreen.parentNode.insertBefore(downloadElement, slide_controller_fullscreen)
   }
 })()
 
-
 function clickDownload() {
-  console.log("クリックイベント発動！！")
+  const buttonHtml = document.getElementById("slide_download").innerHTML
+  document.getElementById("slide_download").innerHTML = '<button class="slide_controller_btn"><i class="fa fa-spinner"></i></button>'
+
   pdfmake.vfs = vfs_fonts.pdfMake.vfs;
   pdfmake.fonts = {
     ipam: {
@@ -36,7 +38,7 @@ function clickDownload() {
     }
   }
 
-  if(document.getElementsByClassName("is-slide")) {
+  if(document.getElementsByClassName("is-slide").length > 0) {
     //タイトルを入れる
     const slideTitle = document.getElementsByClassName("ArticleMainHeader__title")[0].textContent
     const userName   = document.getElementsByClassName("itemsShowAuthorInfo_userName")[0].textContent
@@ -45,12 +47,20 @@ function clickDownload() {
     content.push({text: "by " + userName, fontSize: 25, margin: [0,30,0,0], color: "#999", alignment: 'center', bold: true})
     content.push({text: "", pageBreak: 'before'})
 
-    const slideSection = document.getElementById("item-13a253a081d329d23292")
+    const urlPath = location.pathname
+    const splitUrlPath = urlPath.split("/")
+    console.log(splitUrlPath[splitUrlPath.length -1])
+    const slideSection = document.getElementById("item-" + splitUrlPath[splitUrlPath.length -1])
     const slideSectionChildren = slideSection.childNodes
     slideSectionChildren.forEach((v, k, listObj)=>{
       if(v.tagName == "H2") {
-        content.push({text: v.textContent, fontSize: 25, margin: [0,0,0,10]})
-
+        const nextElement = v.nextElementSibling
+        if(nextElement.tagName == "HR") {
+          //センター
+          content.push({text: v.textContent, fontSize: 50, margin: [0,200,0,10], alignment: 'center', bold: 'true'})
+        }else{
+          content.push({text: v.textContent, fontSize: 25, margin: [0,0,0,10]})
+        }
       }else if(v.tagName == "H3") {
         content.push({text: v.textContent, fontSize: 20, margin: [0,0,0,10]})
 
@@ -61,13 +71,11 @@ function clickDownload() {
           	if(p_v.children.length > 0 && p_v.children[0].tagName == "IMG") {
               //imgタグ
               const img = p_v.children[0]
-              console.log(img.src)
               var req = new XMLHttpRequest();
               req.open('POST', 'https://hxgddvr96i.execute-api.ap-northeast-1.amazonaws.com/prod/encodeImage', false);
               req.setRequestHeader('content-type','application/json')
               req.send(JSON.stringify({url: img.src, width: img.width, height: img.height}))
               if(req.status == 200) {
-                console.log(req.responseText)
                 content.push({image: req.responseText})
                 //content.push({image: ""})
               }
@@ -83,7 +91,6 @@ function clickDownload() {
             content.push({text: p_v.textContent, fontSize: 16, margin: [0, 0], bold: true})
 
           }else if(p_v.nodeType == Node.TEXT_NODE) {
-            console.log(p_v.textContent)
             if(/\r?\n/.test(p_v.textContent) === false) {
               //改行コードの時は何もしない
               content.push({text: p_v.textContent, fontSize: 16, margin: [0, 10]})
@@ -107,7 +114,6 @@ function clickDownload() {
       }else if(v.tagName == "HR"){
         content.push({text: "", pageBreak: 'before'})
       }else if (v.tagName == "IMG"){
-        console.log("imgタグ")
       }
     })
     const docDefinition = { 
@@ -117,7 +123,7 @@ function clickDownload() {
       }
     };
     pdfmake.createPdf(docDefinition).download();
-    //const slides = document.querySelector("section>hr")
+    document.getElementById("slide_download").innerHTML = buttonHtml
   }
   /*
   function imageToBase64(img, mime_type) {
